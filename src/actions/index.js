ed76c0e57dab53c44aa53f0761b975869c4d648b
Brendon <brendon.verissimo@gmail.com>
2017-02-15 21:25:21 -0800

    Add checkauth dispatch

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import fetch from 'whatwg-fetch';
+import * as types from '../constants/ActionTypes';
+import { API } from '../constants/config';
+
+export const checkAuth = () => {
+    dispatch()
+    return dispatch => {
+
+    };
+};
