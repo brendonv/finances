ad54d8859990cedb77a1755877427941ae4f062e
Brendon <brendon.verissimo@gmail.com>
2017-02-15 21:27:00 -0800

    Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,97 @@
-'use strict';
-
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-    devtool: 'eval-source-map',
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        'react-hot-loader/patch',
-        path.join(__dirname, 'src/index.js')
-    ],
-    output: {
-        path: path.join(__dirname, '/dist/'),
-        filename: '[name].js',
-        publicPath: '/'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: 'public/index.html',
-          inject: 'body',
-          filename: 'index.html'
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('development')
-        })
-    ],
-    eslint: {
-        configFile: '.eslintrc',
-        failOnWarning: false,
-        failOnError: false
-    },
-    module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint'
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'babel'
-            },
-            {
-                test: /\.json?$/,
-                loader: 'json'
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
-            },
-            { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-            { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file' }
-        ]
-    }
-};
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, 'src/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    //CUSTOM RUN_TIME ENV VARIABLES.
+    new webpack.DefinePlugin({
+      PORT: JSON.stringify(process.env.PORT) || 5000, 
+      URL: JSON.stringify(process.env.url) || JSON.stringify("http://localhost:" + (process.env.PORT || 5000))
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    }]
+  }
+}
+
+// 'use strict';
+
+// var path = require('path');
+// var webpack = require('webpack');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//     devtool: 'eval-source-map',
+//     entry: [
+//         'webpack-dev-server/client?http://localhost:3000',
+//         'webpack/hot/only-dev-server',
+//         'react-hot-loader/patch',
+//         path.join(__dirname, 'src/index.js')
+//     ],
+//     output: {
+//         path: path.join(__dirname, '/dist/'),
+//         filename: '[name].js',
+//         publicPath: '/'
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//           template: 'public/index.html',
+//           inject: 'body',
+//           filename: 'index.html'
+//         }),
+//         new webpack.optimize.OccurenceOrderPlugin(),
+//         new webpack.HotModuleReplacementPlugin(),
+//         new webpack.NoErrorsPlugin(),
+//         new webpack.DefinePlugin({
+//           'process.env.NODE_ENV': JSON.stringify('development')
+//         })
+//     ],
+//     eslint: {
+//         configFile: '.eslintrc',
+//         failOnWarning: false,
+//         failOnError: false
+//     },
+//     module: {
+//         preLoaders: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 loader: 'eslint'
+//             }
+//         ],
+//         loaders: [
+//             {
+//                 test: /\.js?$/,
+//                 exclude: /node_modules/,
+//                 loader: 'babel'
+//             },
+//             {
+//                 test: /\.json?$/,
+//                 loader: 'json'
+//             },
+//             {
+//                 test: /\.scss$/,
+//                 loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
+//             },
+//             { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+//             { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file' }
+//         ]
+//     }
+// };
