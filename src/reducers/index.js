83b17385527659c56ede4ea9255fe2a989e8eb40
Brendon <brendon.verissimo@gmail.com>
2017-02-15 21:26:26 -0800

    Add reducer signatures

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import * as ActionTypes from '../actions';
+import { combineReducers } from 'redux';
+
+const signup = (state, action) => {
+
+};
+
+const data = (state, action) => {
+
+};
+
+const rootReducer = combineReducers({
+    data
+});
