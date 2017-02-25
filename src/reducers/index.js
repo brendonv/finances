6577eb68caf6513ea7868c098648dada0f540eb2
Brendon Verissimo <brendon.verissimo@nike.com>
2017-02-25 12:41:02 -0800

    Auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,51 @@
-import * as ActionTypes from '../actions';
+import * as ActionTypes from '../constants/actionTypes';
 import { combineReducers } from 'redux';
 
-const signup = (state, action) => {
+const authInitialState = { loggedIn: false, isFetching: false };
+const dataInitialState = { isFetching: false, data: [] };
 
+const auth = (state = authInitialState, action) => {
+	switch (action.type) {
+		case ActionTypes.CHECK_AUTH:
+			return {
+				...state,
+				isFetching: true
+			};
+		case ActionTypes.RECEIVED_AUTH:
+			return {
+				...state,
+				isFetching: false
+			};
+		case ActionTypes.ERROR_AUTH:
+			return {
+				...state,
+				isFetching: false
+			};
+		case ActionTypes.LOGIN:
+			return {
+				...state,
+				isFetching: true
+			};
+		default:
+			return state;
+	}
 };
 
-const data = (state, action) => {
-
+const data = (state = dataInitialState, action) => {
+	switch (action.type) {
+		case ActionTypes.GET_BANK_DATA:
+			return {
+				...state,
+				isFetching: true
+			};
+		default:
+			return state;
+	}
 };
 
 const rootReducer = combineReducers({
+	auth,
     data
 });
+
+export default rootReducer;
