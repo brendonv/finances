5b4603d2688136afd5099a560f1a464f1a5188e5
Brendon Verissimo <brendon.verissimo@nike.com>
2017-02-25 12:39:58 -0800

    Add actions

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -3,3 +3,15 @@ export const SIGNUP = "SIGNUP";
 export const LOGIN = "LOGIN";
 
 export const CHECK_AUTH = "CHECK_AUTH";
+export const RECEIVED_AUTH = "RECEIVED_AUTH";
+export const ERROR_AUTH = "ERROR_AUTH";
+
+export const CHECK_LOGIN = "CHECK_LOGIN";
+export const RECEIVED_LOGIN = "RECEIVED_LOGIN";
+export const ERROR_LOGIN = "ERROR_LOGIN";
+
+export const CHECK_SIGNUP = "CHECK_SIGNUP";
+export const RECEIVED_SIGNUP = "RECEIVED_SIGNUP";
+export const ERROR_SIGNUP = "ERROR_SIGNUP";
+
+export const GET_BANK_DATA = "GET_BANK_DATA";
