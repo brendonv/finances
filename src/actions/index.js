bd1ac5f8ca0db22eb8f7b73c8a5c943246bf9b28
Brendon Verissimo <brendon.verissimo@nike.com>
2017-02-27 17:40:51 -0800

    Get transactions; add actions'

--- src/actions/index.js
+++ src/actions/index.js
@@ -37,12 +37,34 @@ export const errorSignup = () => ({
 	type: types.ERROR_SIGNUP
 });
 
+export const setUser = user => ({
+    type: types.SET_USER,
+    user
+});
+
+export const setAccounts = account => ({
+    type: types.SET_ACCOUNTS,
+    accounts
+});
+
 export const transactionRequest = () => ({
     type: types.GET_TRANSACTIONS
 });
 
 export const getData = () => ({
     type: types.GET_DATA
+});
+
+export const linkAccount = () => ({
+    type: types.LINK_ACCOUNT
+});
+
+export const linkAccountRequest = () => ({
+    type: types.LINK_ACCOUNT_REQUEST
+});
+
+export const linkSuccess = () => ({
+    type: types.LINK_SUCCESS
 });
 
 export const checkAuth = () => dispatch => {
@@ -57,15 +79,16 @@ export const checkAuth = () => dispatch => {
     	.then(json => {
     		console.log("checkauth json", json);
             if (!json.user) {
-                return Promise.reject()
+                return Promise.reject();
             }
     		dispatch(authSuccess());
+            dispatch(setUser(json.user));
             return dispatch(getTransactions(json.user));
     	})
     	.catch(response => {
     		console.log("ERROR", response);
 			return dispatch(authFailed());
-    	})
+    	});
 };
 
 export const login = () => dispatch => {
@@ -78,16 +101,16 @@ export const login = () => dispatch => {
     		return response.json();
     	})
     	.then(json => {
-    		console.log("checkauth json", json);
+    		console.log("login json", json);
     		return dispatch(receivePosts(reddit, json));
     	})
     	.catch(response => {
     		console.log("ERROR", response);
     		if (response.status === 401) {
-    			return dispatch()
+    			return dispatch();
     		}
     		//handle error
-    	})
+    	});
 };
 
 export const signup = () => dispatch => {
@@ -100,21 +123,58 @@ export const signup = () => dispatch => {
     		return response.json();
     	})
     	.then(json => {
-    		console.log("checkauth json", json);
+    		console.log("signup json", json);
     		return dispatch(receivePosts(reddit, json));
     	})
     	.catch(response => {
     		console.log("ERROR", response);
     		if (response.status === 401) {
-    			return dispatch()
+    			return dispatch();
     		}
     		//handle error
-    	})
+    	});
 };
 
 export const getTransactions = user => dispatch => {
     dispatch(transactionRequest());
-    console.log(user);
-    return;
-    return fetch('/')
+    return fetch(`user/${user._id}/transactions`)
+        .then(response => {
+            if (!response.ok) {
+                return Promise.reject(response);
+            }
+            return response.json();
+        })
+        .then(json => {
+            console.log("transaction json", json);
+            return dispatch(receivePosts(reddit, json));
+        })
+        .catch(response => {
+            console.log("ERROR", response);
+            return dispatch(linkAccount());
+        });
 }
+
+export const postLinkAccount = (user, data) => dispatch => {
+    dispatch(linkAccountRequest());
+    return fetch(`user/${user._id}/link`, {
+            method: 'POST',
+            body: {
+                username: "foo",
+                password: "bar"
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                return Promise.reject(response);
+            }
+            return response.json();
+        })
+        .then(json => {
+            console.log("transaction json", json);
+            return dispatch(receivePosts(reddit, json));
+        })
+        .catch(response => {
+            console.log("ERROR", response);
+            //////
+        });
+};
