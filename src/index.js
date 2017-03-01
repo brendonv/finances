69bc786618a512b63a19b5b69a7d98f8e3d1b138
Brendon Verissimo <brendon.verissimo@nike.com>
2017-02-28 17:18:51 -0800

    Add whatwg-fetch

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { checkAuth } from './actions';
 import reducer from './reducers';
 import auth from './middleware/auth';
 import App from './containers/App';
+import 'whatwg-fetch';
 
 const middleware = [ thunk, auth ];
 if (process.env.NODE_ENV !== 'production') {
