e3b47cc931c3beea6a4a21732047068ade3e2088
Brendon Verissimo <brendon.verissimo@nike.com>
2017-03-20 14:55:38 -0700

    Add plaid id and type/s

--- models/category.js
+++ models/category.js
@@ -3,11 +3,10 @@ const Schema   = mongoose.Schema;
 
 
 const categorySchema = new Schema({
-	primary: String,
-	secondary: String
+	plaid_id: { type: String, index: true },
+	type: String,
+	types: Array
 });
-
-categorySchema.index({ primary: 1, secondary: 1 });
 
 const Category = mongoose.model('Category', categorySchema);
 module.exports = Category;
